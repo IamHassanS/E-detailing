1f3e0bf112e7e8c9a1f3b05418159d3dd7e82ae8
sanusers <saneforceapps@gmail.com>
2024-05-29 11:00:54 +0530

    Old source removed - hassan

--- E-Detailing/View Model/FeedbackViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FeedbackViewModel.swift
-//  E-Detailing
-//
-//  Created by SANEFORCE on 17/08/23.
-//
-
-import Foundation
--- E-Detailing/View/ChemistCallCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ChemistCallCell.swift
-//  E-Detailing
-//
-//  Created by SANEFORCE on 27/10/23.
-//
-
-import Foundation
